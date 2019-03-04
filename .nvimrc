4030de2a3a4adc0d48238544b22a4969d9d44863
Jakob <jakob.lindqvist@chalmers.se>
2019-03-04 11:28:59 +0100

    Config parser basics. Threads not done yet

--- .nvimrc
+++
@@ -1,4 +0,0 @@
-"let g:gutentags_project_root = ['Cargo.toml']
-"call add(g:gutentags_project_info, {'type': 'rust', 'file': 'Cargo.toml'})
-let g:gutentags_project_info = []
-call add(g:gutentags_project_info, {'type': 'rust', 'file': 'Cargo.toml'})
--- src/config.rs
+++ src/config.rs
@@ -5,27 +5,15 @@ use toml;
 
 #[derive(Deserialize)]
 pub struct Config {
-    path: &'static str,
+    name: String,
 }
 
 impl Config {
     pub fn new(config_file: &'static str) -> Config {
-        //let mut file = match File::open(config_file) {
-        //    Ok(file) => file,
-        //    Err(_) => panic!("Could not find config file"),
-        //};
-
-        //let mut config_toml = String::new();
-        //file.read_to_string(&mut config_toml).expect("Wrong");
-
-        //let config: Config = toml::from_str(&config_toml).unwrap();
-        //println!("config_toml");
-        //config
-        let mut f = File::open("/etc/hosts").expect("Unable to open file");
+        let mut f = File::open(config_file).expect("Unable to open file");
         let mut data = String::new();
         f.read_to_string(&mut data).expect("Unable to read string");
-        let config: Config = toml::from_str(&data).unwrap();
-        println!("{}", data);
+        let config: Config = toml::de::from_str(&data).unwrap();
         config
     }
 }
