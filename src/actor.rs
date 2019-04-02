57345cd3bb65175fa2bebd668958dc2d0bc537c3
Jakob <jakob.lindqvist@chalmers.se>
2019-04-02 09:52:43 +0200

    Started xor actor

--- src/actor.rs
+++ src/actor.rs
@@ -5,11 +5,13 @@ pub struct DummyActor {
     pub id: &'static str,
 }
 
-// TODO: String to something like a'<static>&str
-impl Actor for DummyActor {
-    fn new(id: &'static str, _: Option<u32>) -> DummyActor {
+impl DummyActor {
+    pub fn new(id: &'static str, _: Option<u32>) -> DummyActor {
         DummyActor { id: id }
     }
+}
+
+impl Actor for DummyActor {
     fn set_power(&self, power: f32) -> Result<(), gpio_cdev::errors::Error> {
         Ok(())
     }
@@ -20,8 +22,8 @@ pub struct SimpleGpio {
     handle: gpio_cdev::LineHandle,
 }
 
-impl Actor for SimpleGpio {
-    fn new(id: &'static str, pin_number: Option<u32>) -> SimpleGpio {
+impl SimpleGpio {
+    pub fn new(id: &'static str, pin_number: Option<u32>) -> SimpleGpio {
         let pin_number = pin_number.unwrap();
         let handle = match hardware::get_gpio_handle("/dev/gpiochip0", pin_number, &id) {
             Ok(handle) => handle,
@@ -34,6 +36,43 @@ impl Actor for SimpleGpio {
             handle: handle,
         }
     }
+}
+
+impl Actor for SimpleGpio {
+    fn set_power(&self, power: f32) -> Result<(), gpio_cdev::errors::Error> {
+        let gpio_state = match power {
+            power if power > 0.0 => 1,
+            power if power <= 0.0 => 0,
+            _ => 0,
+        };
+        self.handle.set_value(gpio_state)
+    }
+}
+
+pub struct XorActor {
+    pub own_id: &'static str,
+    pub paired_id: &'static str,
+    handle: gpio_cdev::LineHandle,
+}
+
+impl XorActor {
+    pub fn new(own_id: &'static str, paired_id: &'static str, pin_number: Option<u32>) -> XorActor {
+        let pin_number = pin_number.unwrap();
+        let handle = match hardware::get_gpio_handle("/dev/gpiochip0", pin_number, &own_id) {
+            Ok(handle) => handle,
+            Err(e) => {
+                panic!("Could not get handle, {}.", e);
+            }
+        };
+        XorActor {
+            own_id: own_id,
+            paired_id: paired_id,
+            handle: handle,
+        }
+    }
+}
+
+impl Actor for XorActor {
     fn set_power(&self, power: f32) -> Result<(), gpio_cdev::errors::Error> {
         let gpio_state = match power {
             power if power > 0.0 => 1,
@@ -45,6 +84,5 @@ impl Actor for SimpleGpio {
 }
 
 pub trait Actor {
-    fn new(id: &'static str, pin_number: Option<u32>) -> Self;
     fn set_power(&self, power: f32) -> Result<(), gpio_cdev::errors::Error>;
 }
