7c30cb82971a7379d4a99aa92934c4dd928c7ff4
Jakob <jakob.lindqvist@chalmers.se>
2020-03-11 13:59:39 +0100

    Moving fun's main -> lib.
    Generic can-kicking for `send`

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,12 +1,29 @@
+use crate::opts::Opt;
 use bryggio_lib::api;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use serde_json;
+use std::io::Write;
 use ureq;
 use url::Url;
 pub mod opts;
 
 /// TODO: Generic response
-pub fn send(request: &Url) -> Result<api::Response<f32>, serde_json::error::Error> {
+pub fn send<T>(request: &Url) -> Result<api::Response<T>, serde_json::error::Error>
+where
+    T: Serialize + DeserializeOwned,
+{
     println!("Sending request: '{}'.", request);
-    let response_raw = ureq::get(request.as_str()).call();
-    serde_json::from_str(&response_raw.into_string().unwrap())
+    let response_raw = ureq::get(request.as_str()).call().into_string().unwrap();
+    serde_json::from_str(&response_raw)
+}
+
+pub fn init_logging(opt: &Opt) {
+    let mut builder = env_logger::Builder::from_default_env();
+    if opt.verbose() {
+        builder.filter(None, log::LevelFilter::Debug);
+    }
+    builder
+        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .init();
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,13 +2,12 @@ use bryggio_cli;
 use bryggio_cli::opts::Opt;
 use env_logger;
 use log::{debug, error, info};
-use std::io::Write;
 use structopt::StructOpt;
 
 fn run_subcommand(opt: Opt) {
     match opt {
         Opt::Brewery(options) => {
-            match bryggio_cli::send(&options.url().unwrap()) {
+            match bryggio_cli::send::<f32>(&options.url().unwrap()) {
                 Ok(response) => {
                     if response.success {
                         match response.result {
@@ -30,18 +29,8 @@ fn run_subcommand(opt: Opt) {
     }
 }
 
-fn init_logging(opt: &Opt) {
-    let mut builder = env_logger::Builder::from_default_env();
-    if opt.verbose() {
-        builder.filter(None, log::LevelFilter::Debug);
-    }
-    builder
-        .format(|buf, record| writeln!(buf, "{}", record.args()))
-        .init();
-}
-
 fn main() {
     let opt = Opt::from_args();
-    init_logging(&opt);
+    bryggio_cli::init_logging(&opt);
     run_subcommand(opt)
 }
