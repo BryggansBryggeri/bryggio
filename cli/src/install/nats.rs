4dedb7ae4a018c6657cdee1226f3cef2c1679372
Jakob <jakob.lindqvist@chalmers.se>
2021-03-15 20:10:00 +0100

    Zip file extraction of server

--- cli/src/install/nats.rs
+++ cli/src/install/nats.rs
@@ -4,7 +4,9 @@ use crate::install::{
 };
 use log::info;
 use semver::Version;
-use std::path::Path;
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
 use url::Url;
 
 pub(crate) fn setup_nats_server(nats_path: &Path, update: bool) {
@@ -16,9 +18,29 @@ pub(crate) fn setup_nats_server(nats_path: &Path, update: bool) {
     }
     let (latest_nats_version, nats_url) = github_meta_data();
     if should_download(update, local_version, latest_nats_version) {
-        download_file(&nats_path, &nats_url);
+        let zip_path = nats_path.with_extension("zip");
+        download_file(&zip_path, &nats_url);
+        extract_server(&zip_path);
         make_executable(nats_path);
     }
+}
+
+fn extract_server(zip_path: &Path) {
+    let file = fs::File::open(&zip_path).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    let mut nats_server_name = PathBuf::new();
+    for i in 0..archive.len() {
+        let file_ = archive.by_index(i).unwrap();
+        let file_name = file_.enclosed_name().unwrap().to_owned();
+        if file_name.file_stem().unwrap() == "nats-server" {
+            nats_server_name = file_name;
+        }
+    }
+    println!("FILE: {:?}", nats_server_name);
+    let mut extracted_file = archive.by_name(nats_server_name.to_str().unwrap()).unwrap();
+    let mut outfile = fs::File::create(&zip_path.with_file_name("nats-server")).unwrap();
+    println!("OUT: {:?}", outfile);
+    io::copy(&mut extracted_file, &mut outfile).unwrap();
 }
 
 fn github_meta_data() -> (Version, Url) {
